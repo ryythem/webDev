be2114975950266271c4875b0f0aa0ded06b986f
RythemGoyal <imrythem16@gmail.com>
2024-11-15 19:27:55 +0530

    useMemo

--- 7.class-6/class-6.2/src/App.jsx
+++ 7.class-6/class-6.2/src/App.jsx
@@ -1,23 +1,33 @@
-import { useEffect, useMemo, useState } from "react";
+import { useEffect, memo, useState, useCallback } from "react";
 
 function App() {
   const [count, setCount] = useState(0);
-  const [sum, setSum] = useState(1);
-  let countt = useMemo(() => {
-    let countt = 0;
-    for (let i = 0; i <= sum; i++) {
-      countt = countt + i;
-    }
-    return countt;
-  }, [sum]);
+
+  const logSomething = useCallback(() => {
+    console.log("child clicked"); 
+  }, []);
 
   return (
     <div>
-      <input type="text" onChange={(e) => setSum(e.target.value)} />
-      <p>Sum is {countt}</p>
-      <button onClick={() => setCount(count + 1)}>Counter ({count})</button>
+      <ButtonComponent inputFuntion={logSomething} />
+      <button
+        onClick={() => {
+          setCount(count + 1);
+        }}
+      >
+        Click me {count}
+      </button>
     </div>
   );
 }
+const ButtonComponent = memo(({ inputFuntion }) => {
+  console.log("child render");
+
+  return (
+    <div>
+      <button onClick={inputFuntion}>Button clicked</button>
+    </div>
+  );
+});
 
 export default App;
