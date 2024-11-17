58ffe36257f2ada81a6497ae32529a39b3ea01a9
RythemGoyal <imrythem16@gmail.com>
2024-11-17 19:44:51 +0530

    react-router-dom

---
+++ 8.class-7/class-7.1/src/App.jsx
@@ -0,0 +1,48 @@
+import React, { Suspense, useState } from "react";
+import { BrowserRouter, Routes, Route, useNavigate } from "react-router-dom";
+import Landing from "./components/Landing";
+const Dashboard = React.lazy(() => import("./components/Dashboard"));
+function App() {
+  return (
+    <div>
+      <BrowserRouter>
+        <Appbar />
+        <Routes>
+          <Route
+            path="/dashboard"
+            element={
+              <Suspense fallback={"loading "}>
+                <Dashboard />
+              </Suspense>
+            }
+          />
+          <Route path="/" element={<Landing />} />
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
+}
+
+function Appbar() {
+  const navigate = useNavigate();
+  return (
+    <div>
+      <button
+        onClick={() => {
+          navigate("/");
+        }}
+      >
+        Landing Page
+      </button>
+      <button
+        onClick={() => {
+          navigate("/dashboard");
+        }}
+      >
+        Dashboard
+      </button>
+    </div>
+  );
+}
+
+export default App;
