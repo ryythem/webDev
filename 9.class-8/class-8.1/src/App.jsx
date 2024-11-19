f0c32cdca90aa05b899de5a12a2a7f1eb6b59023
RythemGoyal <imrythem16@gmail.com>
2024-11-19 17:20:21 +0530

    Tailwind css

---
+++ 9.class-8/class-8.1/src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from "react";
+
+function App() {
+  return (
+    <>
+      <div className="grid grid-cols-1 md:grid-cols-3 ">
+        <div className="bg-red-500 ">hi</div>
+        <div className="bg-blue-500 ">hi</div>
+        <div className="bg-green-500 ">hi</div>
+      </div>
+    </>
+  );
+}
+
+export default App;
