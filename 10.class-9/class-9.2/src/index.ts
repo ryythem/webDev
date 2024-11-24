bc711dcc50b2e2558e38795527049b359bc76d5c
RythemGoyal <imrythem16@gmail.com>
2024-11-24 17:07:18 +0530

    Enums

--- 10.class-9/class-9.2/src/index.ts
+++ 10.class-9/class-9.2/src/index.ts
@@ -1,62 +1,7 @@
-type User = {
-  firstName: string;
-  lastName: string;
-  age: number;
-  email?: string;
-};
-
-function isLegal(user: User) {
-  if (user.age >= 18) {
-    return true;
-  } else {
-    return false;
-  }
+function getFirstElement<T>(arg: T[]) {
+  return arg[0];
 }
 
-function greet(user: User) {
-  console.log("Hello " + user.firstName);
-}
-
-isLegal({
-  firstName: "rythem",
-  lastName: "goyal",
-  age: 20,
-  email: "asdjh",
-});
-
-function maxValue(arr: number[]): number {
-  let max = 0;
-  for (let i = 0; i < arr.length; i++) {
-    if (arr[i] > max) {
-      max = arr[i];
-    }
-  }
-  return max;
-}
-
-console.log(maxValue([1, 2, 3]));
-
-interface User1 {
-  firstName: string;
-  lastName: string;
-  age: number;
-}
-
-function filteredUser(users: User1[]) {
-  return users.filter((x) => x.age >= 18);
-}
-
-console.log(
-  filteredUser([
-    {
-      firstName: "rythem",
-      lastName: "goyal",
-      age: 20,
-    },
-    {
-      firstName: "ishika",
-      lastName: "agarwal",
-      age: 18,
-    },
-  ])
-);
+const el = getFirstElement<string>(["rythem", "goyal"]);
+const el1 = getFirstElement<number>([1, 2, 3]);
+console.log(el.toUpperCase());
