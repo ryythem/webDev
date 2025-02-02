7c4480fdd68d01be645cec869066e669ebb5e224
RythemGoyal <imrythem16@gmail.com>
2025-02-02 16:52:40 +0530

    Nextjs backend

---
+++ class-14/next-backend/app/api/user/route.ts
@@ -0,0 +1,16 @@
+import { NextRequest } from "next/server";
+import { PrismaClient } from "@prisma/client";
+const client = new PrismaClient();
+export async function POST(req: NextRequest) {
+  const body = await req.json();
+  await client.user.create({
+    data: {
+      username: body.username,
+      password: body.username,
+    },
+  });
+
+  return Response.json({
+    message: "You are logged in",
+  });
+}
---
+++ class-14/next-backend/app/page.tsx
@@ -0,0 +1,5 @@
+import axios from "axios";
+
+export default async function Home() {
+  return <div className="flex flex-col justify-center h-screen">hi there</div>;
+}
