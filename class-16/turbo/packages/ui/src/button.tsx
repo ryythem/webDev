95e35564620981403e8d88e5fcecc9d75f072022
RythemGoyal <imrythem16@gmail.com>
2025-02-07 19:57:53 +0530

    turborepo intro

--- class-16/turbo/packages/ui/src/button.tsx
+++ class-16/turbo/packages/ui/src/button.tsx
@@ -2,19 +2,14 @@
 
 import { ReactNode } from "react";
 
-interface ButtonProps {
-  children: ReactNode;
-  className?: string;
-  appName: string;
-}
 
-export const Button = ({ children, className, appName }: ButtonProps) => {
+
+export const Button =() => {
   return (
     <button
-      className={className}
-      onClick={() => alert(`Hello from your ${appName} app!`)}
+      onClick={() => alert(`Hello from your rythem app!`)}
     >
-      {children}
+      {}
     </button>
   );
 };
