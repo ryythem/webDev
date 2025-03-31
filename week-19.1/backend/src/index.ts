1b1d96701deff16f1036812bce9bdc2b4c52c53a
RythemGoyal <imrythem16@gmail.com>
2025-03-31 23:44:55 +0530

    Learning web Sockets

---
+++ week-19.1/backend/src/index.ts
@@ -0,0 +1,20 @@
+import { WebSocketServer, WebSocket } from "ws";
+
+const wss = new WebSocketServer({ port: 8000 });
+
+let userCount = 0;
+let allSockets: WebSocket[] = [];
+
+wss.on("connection", (socket) => {
+  allSockets.push(socket);
+  userCount = userCount + 1;
+  console.log("User connected #" + userCount);
+
+  socket.on("message", (message) => {
+    console.log("message received " + message.toString());
+    for (let i = 0; i < allSockets.length; i++) {
+      const s = allSockets[i];
+      s.send(message + ": sent from the server");
+    }
+  });
+});
